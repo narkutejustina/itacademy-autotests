39b9a95880496c9570b5b1914b77bda56d378dfa
p998jfn <p998jfn@myntet.se>
2016-03-03 18:06:26 +0200

    Done testing

--- src/SampleTest.java
+++ src/SampleTest.java
@@ -4,30 +4,54 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.io.File;
 
 public class SampleTest {
 
     @Test
-    public void testCase() {
-        File file = new File("C:/IEDriverServer.exe");
+    public void testCase() throws InterruptedException {
+        File file = new File("C:/Users/p998jfn/IdeaProjects/itacademy-autotests/chromedriver.exe");
         System.setProperty("webdriver.ie.driver", file.getAbsolutePath());
         WebDriver driver = new InternetExplorerDriver();
-        driver.get("https://www.swedbank.lt/lt/spreadsheets/consumer2");
-        WebElement firstInput = driver.findElement(By.id("SpreadsheetRequestedLoanAmount"));
-        WebElement secondInput = driver.findElement(By.id("SpreadsheetNetMonthlyIncome"));
-        WebElement firstRadio = driver.findElement(By.id("SpreadsheetWageAccount1"));
-        WebElement secondRadio = driver.findElement(By.id("SpreadsheetHasOutstandingLoan1"));
-        firstInput.clear();
-        firstInput.sendKeys("500");
-        secondInput.clear();
-        secondInput.sendKeys("125");
-        firstRadio.click();
-        secondRadio.click();
-        secondRadio.submit();
-        WebElement ActualSum = driver.findElement(By.xpath("//div[contains(@class, 'consumerResultValue')]"));
-        Assert.assertEquals(ActualSum.getText(), "14,48 Eur");
+        driver.get("http://contactform-swedbank.rhcloud.com/registration.html");
+
+        WebElement surname = driver.findElement(By.id("inputSurname"));
+        WebElement phone = driver.findElement(By.id("inputPhone"));
+
+        WebElement bankDivision = driver.findElement(By.id("inputBankDivision"));
+        WebElement button = driver.findElement(By.xpath("//input[contains(@value, 'Register')]"));
+        WebElement name = driver.findElement(By.id("inputName"));
+        WebElement en = driver.findElement(By.id("en"));
+        WebElement lt = driver.findElement(By.id("lt"));
+        WebElement date = driver.findElement(By.id("inputDate"));
+        WebElement topic = driver.findElement(By.id("inputTopic"));
+        WebElement email = driver.findElement(By.id("inputEmail"));
+        WebElement comment = driver.findElement(By.id("inputOther"));
+
+        button.click();
+        en.click();
+        lt.click();
+        name.sendKeys("Vardas");
+        surname.sendKeys("Pavarde");
+        phone.sendKeys("860000000");
+        date.sendKeys("2016-03-28 12:00");
+        email.sendKeys("email@email.com");
+        comment.sendKeys("komentarai");
+
+        Select selectBank = new Select(bankDivision);
+        Select selectTopic = new Select(topic);
+        selectBank.selectByValue("Gedimino pr. 56");
+        selectTopic.selectByValue("Taupymas ir investavimas");
+
+        Thread.sleep(3000);
+
+        WebElement submit = driver.findElement(By.xpath("//input[contains(@value, 'Siųsti')]"));
+        submit.click();
+
+        WebElement result = driver.findElement(By.xpath("//a[contains(@class,'col-xs-offset-10 col-xs-2 btn btn-primary ng-binding')]"));
+        Assert.assertEquals(result.getAttribute("href"), "http://contactform-swedbank.rhcloud.com/index.html");
         driver.quit();
     }
 }
